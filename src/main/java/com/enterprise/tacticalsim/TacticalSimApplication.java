9e729b0fcdd3ae8a582b8b5409d12682083f3d22
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-09-14 16:01:37 -0500

    feat: crear clase principal de arranque TacticalSimApplication con anotacion EnableAsync

--- src/main/java/com/enterprise/tacticalsim/TacticalSimApplication.java
+++ src/main/java/com/enterprise/tacticalsim/TacticalSimApplication.java
@@ -1,4 +1,14 @@
 package com.enterprise.tacticalsim;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
 public class TacticalSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacticalSimApplication.class, args);
+    }
 }
