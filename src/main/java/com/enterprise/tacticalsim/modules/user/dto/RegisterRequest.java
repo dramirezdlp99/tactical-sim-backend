40d946eaf7dd20992cc9c8e50d08981ef8e4932e
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-09-15 08:52:44 -0500

    feat: crear DTO RegisterRequest para la validacion de nuevos usuarios

---
+++ src/main/java/com/enterprise/tacticalsim/modules/user/dto/RegisterRequest.java
@@ -0,0 +1,37 @@
+package com.enterprise.tacticalsim.modules.user.dto;
+
+import com.enterprise.tacticalsim.modules.user.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name cannot exceed 50 characters")
+    private String name;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name cannot exceed 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @NotNull(message = "Role is required")
+    private Role role;
+}
