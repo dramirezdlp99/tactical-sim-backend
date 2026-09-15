d5ccb6d650b8bb6572f4d5fcbf8cc02309eebbba
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-09-15 08:53:09 -0500

    feat: crear DTO AuthResponse para enviar tokens JWT y datos de sesion

---
+++ src/main/java/com/enterprise/tacticalsim/modules/user/dto/AuthResponse.java
@@ -0,0 +1,20 @@
+package com.enterprise.tacticalsim.modules.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long userId;
+    private String email;
+    private String role;
+}
